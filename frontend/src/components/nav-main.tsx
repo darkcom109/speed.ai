8069d1a9ccdf5f36ef33b243bb5b2058b1166d5a
darkcom109 <garciavalentino2006@gmail.com>
2026-05-27 21:24:09 +0100

    chore: fix navigation bar to only show subpages on click

--- frontend/src/components/nav-main.tsx
+++ frontend/src/components/nav-main.tsx
@@ -9,7 +9,7 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
-import { NavLink } from "react-router"
+import { NavLink, useLocation } from "react-router"
 
 export function NavMain({
   groups,
@@ -27,6 +27,8 @@ export function NavMain({
     }[]
   }[]
 }) {
+  const location = useLocation()
+
   return (
     <>
       {groups.map((group) => (
@@ -36,32 +38,45 @@ export function NavMain({
             <SidebarMenu>
               {group.items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <NavLink to={item.url}>
-                    {({ isActive }) => (
-                      <SidebarMenuButton
-                        isActive={item.items ? false : isActive}
-                        tooltip={item.title}
-                      >
-                        {item.icon}
-                        <span>{item.title}</span>
-                      </SidebarMenuButton>
-                    )}
-                  </NavLink>
-                  {item.items && (
-                    <SidebarMenuSub>
-                      {item.items.map((subItem) => (
-                        <SidebarMenuSubItem key={subItem.title}>
-                          <NavLink to={subItem.url}>
-                            {({ isActive }) => (
-                              <SidebarMenuSubButton isActive={isActive}>
-                                <span>{subItem.title}</span>
-                              </SidebarMenuSubButton>
-                            )}
-                          </NavLink>
-                        </SidebarMenuSubItem>
-                      ))}
-                    </SidebarMenuSub>
-                  )}
+                  {(() => {
+                    const isSubItemActive = item.items?.some((subItem) => {
+                      return location.pathname === subItem.url
+                    })
+                    const shouldShowSubItems = Boolean(
+                      item.items && isSubItemActive
+                    )
+
+                    return (
+                      <>
+                        <NavLink to={item.url}>
+                          {({ isActive }) => (
+                            <SidebarMenuButton
+                              isActive={item.items ? false : isActive}
+                              tooltip={item.title}
+                            >
+                              {item.icon}
+                              <span>{item.title}</span>
+                            </SidebarMenuButton>
+                          )}
+                        </NavLink>
+                        {shouldShowSubItems && (
+                          <SidebarMenuSub>
+                            {item.items?.map((subItem) => (
+                              <SidebarMenuSubItem key={subItem.title}>
+                                <NavLink to={subItem.url}>
+                                  {({ isActive }) => (
+                                    <SidebarMenuSubButton isActive={isActive}>
+                                      <span>{subItem.title}</span>
+                                    </SidebarMenuSubButton>
+                                  )}
+                                </NavLink>
+                              </SidebarMenuSubItem>
+                            ))}
+                          </SidebarMenuSub>
+                        )}
+                      </>
+                    )
+                  })()}
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
