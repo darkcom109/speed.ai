993e4e7de21d9397c9e97c760f6f0fe654b2c1d8
darkcom109 <garciavalentino2006@gmail.com>
2026-05-27 12:38:38 +0100

    feat: add user data to profile section

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -21,10 +21,21 @@ import {
   SquareCheckBigIcon,
 } from "lucide-react"
 
+async function getUserData() {
+  const response = await fetch("http://localhost:3001/api/auth/me", {
+    credentials: "include"
+  })
+  const data = await response.json()
+
+  return data
+}
+
+const userData = await getUserData()
+
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: userData.user.name,
+    email: userData.user.email,
     avatar: "/avatars/shadcn.jpg",
   },
   navMain: [
