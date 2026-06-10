560fefdd7b9594c30040a05a19e07ef7eee78f3a
darkcom109 <garciavalentino2006@gmail.com>
2026-06-10 01:30:01 +0100

    chore: add validator for ensuring user contains 3 months of data for accuracy

--- backend/src/routes/future-projection-routes/forecast-algorithms/linear-regression-route.js
+++ backend/src/routes/future-projection-routes/forecast-algorithms/linear-regression-route.js
@@ -81,6 +81,16 @@ futurePredictionRouter.get("/forecast-regression", async (req, res) => {
             }
         })
 
+        const hasThreeMonthsOfData = monthlyData.every((month) => {
+            return month.income > 0 || month.expense > 0
+        })
+
+        if (!hasThreeMonthsOfData) {
+            return res.status(400).json({
+                error: "At least 3 months of data is required to build forecase"
+            })
+        }
+
         const points = monthlyData.map((month, index) => {
             return {
                 x: index,
